6239433f10c222b54e6e6762e0008edcb301878f
mehmetozanguven <mehmetozanguven@gmail.com>
2019-10-05 10:46:38 +0300

    addQuestionToStudent test cases

--- src/test/java/com/myProjects/soru_cozum/services/StudentServiceTests.java
+++ src/test/java/com/myProjects/soru_cozum/services/StudentServiceTests.java
@@ -4,24 +4,14 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.when;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.myProjects.soru_cozum.enums.Department;
 import com.myProjects.soru_cozum.model.Publisher;
@@ -29,8 +19,6 @@ import com.myProjects.soru_cozum.model.Question;
 import com.myProjects.soru_cozum.model.Student;
 import com.myProjects.soru_cozum.model.StudentDetails;
 import com.myProjects.soru_cozum.repository.StudentDAO;
-import com.myProjects.soru_cozum.repository.StudentDAOImpl;
-import com.myProjects.soru_cozum.service.StudentService;
 import com.myProjects.soru_cozum.service.StudentServiceImpl;
 
 public class StudentServiceTests {
@@ -131,15 +119,28 @@ public class StudentServiceTests {
 		String questionSubCategory = "Türev";
 		Publisher tempPublisher = new Publisher();
 		tempPublisher.setId((long)1);
-		Question tempQuestion = createTempQuestion(pageNumber, questionNumber, questionCategory, questionSubCategory,
-				tempPublisher);
+
 		
 		Student student = new Student();
 		student.setId((long) 1);
 		
 		Optional<Question> found = studentService.isStudentAskedThatQuestionBefore(student, tempPublisher, pageNumber, questionNumber, questionCategory, questionSubCategory);
 		
-		assertNotEquals(student.getId(), found.get().getId());
+		assertEquals(true, found.isEmpty());
+	}
+	
+	@Test
+	public void addQuestionToStudentWithoutCreatingNewQuestion_test() {
+		Student student = new Student();
+		student.setId((long) 1);
+		int beforeQuestionNum = student.getStudentQuestions().size();
+		
+		Question tempQuestion = new Question();
+		tempQuestion.setId((long)1);
+		studentService.addQuestionToStudentWithoutCreatingNewQuestion(tempQuestion, student);
+		int afterQuestionNum = student.getStudentQuestions().size();
+		
+		assertEquals(beforeQuestionNum+1, afterQuestionNum);
 	}
 
 	private Question createTempQuestion(int pageNumber, int questionNumber, String questionCategory,
