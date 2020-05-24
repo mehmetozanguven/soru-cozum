449f6ee4b75d94fbbe540f4d78b66cb5ac126dc3
mehmetozanguven <mehmetozanguven@gmail.com>
2020-05-24 23:11:13 +0300

    cors trial

--- src/main/java/com/myProjects/soru_cozum/controller/SingupController.java
+++ src/main/java/com/myProjects/soru_cozum/controller/SingupController.java
@@ -6,10 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.myProjects.soru_cozum.chainPattern.signup.student.CreateNewStudentDetailsHandler;
 import com.myProjects.soru_cozum.chainPattern.signup.student.CreateNewStudentHandler;
@@ -32,6 +29,7 @@ import com.myProjects.soru_cozum.service.TeacherService;
  */
 @RestController()
 @RequestMapping("/signup")
+@CrossOrigin(origins = "http://localhost", maxAge = 3600)
 public class SingupController {
 	private final Logger LOGGER = LoggerFactory.getLogger(SingupController.class);
 	
