1c3426d0eaf1e3f9e76244e6826a55dfd3df77d7
mehmetozanguven <mehmetozanguven@gmail.com>
2020-05-24 23:30:33 +0300

    cors v4

---
+++ src/main/java/com/myProjects/soru_cozum/security/CorsFilter.java
@@ -0,0 +1,17 @@
+package com.myProjects.soru_cozum.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter  extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
